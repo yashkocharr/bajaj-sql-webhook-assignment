6cd330b6ece16644fda51c0d7ba39f6cb8e0b09e
YashKochar <YashKochar04@gmail.com>
2025-07-29 20:47:50 +0530

    Final working code for Bajaj Finserv assignment

---
+++ src/main/java/com/bajaj/sqlhook/SqlWebhookBajajApplication.java
@@ -0,0 +1,13 @@
+package com.bajaj.sqlhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlWebhookBajajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlWebhookBajajApplication.class, args);
+	}
+
+}
